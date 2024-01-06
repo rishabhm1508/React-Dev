4ef92efc460dc2ddf664ee97604095f2eff8baa1
Rishabh Mishra <rishabhm1508@gmail.com>
2024-01-07 00:19:27 +0530

    Functional component and Element Rendering as JS Code

--- app.js
+++ app.js
@@ -7,9 +7,28 @@ import ReactDOM from "react-dom/client";
 //   React.createElement("h2", { id: "h2Tag" }, "H2 tag is in action"),
 // ]);
 
-const parent = <div id="hiTag"> This is React from JSX..!!</div>;
+// JSX Code
+// const parent = <div id="hiTag"> This is React from JSX..!!</div>;
 
-console.log(parent);
+// Functional Component and Functional component composition.
+const RenderEle = () => <h2 id="h2Tag">Render component in element...!!!</h2>;
+
+const title = (
+  <h1 className="dummy-classs">
+    <RenderEle />
+    This is coming from Child..!!!
+  </h1>
+);
+
+const Parent = () => {
+  return (
+    <div id="some">
+      <h1 id="hiTag" className="dummy-classs">
+        This is coming from Parent...!!!
+      </h1>
+      {title}
+    </div>
+  );
+};
 const renderer = ReactDOM.createRoot(document.getElementById("root"));
-
-renderer.render(parent);
+renderer.render(<Parent />);
