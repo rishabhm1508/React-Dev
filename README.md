31398770376220754d4f0c8b81f1c605afcb9ec5
Rishabh Mishra <rishabhm1508@gmail.com>
2024-01-06 23:35:39 +0530

    Added first JSX code and package.json

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # React Playground 🔥
 
 React foundations and some playaround
+
+# Parcel
+
+- Build App
+- Local Server
+- HMR - Hot Module Replacement
+- Has File Watching algo
+- Caching for faster rebuilds
+- Image Optimization
+- Minification
+- Bundling
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - Browser support
+- Error Handling
+- Tree shaking
+- Https support
+- Different prod and dev builds ( Prod build take more time )
--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const parent = React.createElement("div", { id: "child" }, [
-  React.createElement("h1", { id: "hiTag" }, "H1 tag is in action"),
-  React.createElement("h2", { id: "h2Tag" }, "H2 tag is in action"),
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Original React script
+// const parent = React.createElement("div", { id: "child" }, [
+//   React.createElement("h1", { id: "hiTag" }, "H1 tag is in action"),
+//   React.createElement("h2", { id: "h2Tag" }, "H2 tag is in action"),
+// ]);
+
+const parent = <div id="hiTag"> This is React from JSX..!!</div>;
 
 console.log(parent);
 const renderer = ReactDOM.createRoot(document.getElementById("root"));
