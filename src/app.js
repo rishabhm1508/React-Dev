91d3d4cb2f02025bd4c32323e565572c8f5bf706
Rishabh Mishra <rishabhm1508@gmail.com>
2024-09-29 13:22:40 +0530

    remove dead code

--- src/app.js
+++ src/app.js
@@ -2,16 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import HeaderComponent from "./Header";
 import BodyComponent from "./Body";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet,
-  BrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Contact from "./Contact";
 import Error from "./Error";
 import RestaurantDetailsCard from "./RestaurantDetailsCard";
-import ContactClassBased from "./Contact-ClassBased";
 import { Provider } from "react-redux";
 import appStore from "./store/appStore";
 import CartComponent from "./Cart";
